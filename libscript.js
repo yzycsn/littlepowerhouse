a80f0b8a8e0ad5758f973cee977c9cbfaa03312f
yzycsn <154832604+yzycsn@users.noreply.github.com>
2023-12-30 18:59:30 -0500

    Update libscript.js

--- libscript.js
+++ libscript.js
@@ -2,7 +2,7 @@ let slideIndex = 0;
 
 function showSlides() {
     const slidesContainer = document.getElementById("slideshow-container");
-    //slidesContainer.innerHTML = ''; // Clear existing content
+    slidesContainer.innerHTML = ''; // Clear existing content
 
     // Array of book covers (replace with actual cover filenames)
     const bookCovers = ['book1_cover.jpg', 'book2_cover.jpg'];
@@ -16,22 +16,39 @@ function showSlides() {
         img.alt = `Book ${i + 1} Cover`;
 
         // Set a fixed height for each book cover based on the aspect ratio (1:1.41)
-        img.style.height = 'calc(100vw / 1.41)';
-
-        // Add click event listener to redirect to the corresponding PDF file
-        img.addEventListener('click', function() {
-            window.open(`books/book${i + 1}.pdf`, '_blank');
-        });
-
-        const bookTitle = document.createElement('div');
-        bookTitle.classList.add('book-title');
-        bookTitle.innerHTML = `<a href="books/book${i + 1}.pdf" target="_blank">Book ${i + 1}</a>`;
+        img.style.height = 'calc(10vw / 1.41)';
 
         slide.appendChild(img);
-        slide.appendChild(bookTitle);
-
         slidesContainer.appendChild(slide);
     }
 
-    //
+    // Display the current slide
+    slidesContainer.getElementsByClassName('book-slide')[slideIndex].style.display = 'block';
+}
 
+function plusSlides(n) {
+    slideIndex += n;
+
+    const slides = document.getElementsByClassName('book-slide');
+
+    if (slideIndex >= slides.length) {
+        slideIndex = 0;
+    } else if (slideIndex < 0) {
+        slideIndex = slides.length - 1;
+    }
+
+    showSlides();
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    showSlides();
+
+    // Attach click event handlers to navigation arrows
+    document.querySelector('.prev').addEventListener('click', function() {
+        plusSlides(-1);
+    });
+
+    document.querySelector('.next').addEventListener('click', function() {
+        plusSlides(1);
+    });
+});
