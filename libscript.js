2f9f653af252dbffe80e0a1a85f78d8e376b8932
yzycsn <154832604+yzycsn@users.noreply.github.com>
2023-12-30 18:28:29 -0500

    Update libscript.js

--- libscript.js
+++ libscript.js
@@ -1,19 +1,38 @@
 let slideIndex = 0;
 
 function showSlides() {
-    const slides = document.getElementsByClassName("book-slide");
+    const slidesContainer = document.getElementById("slideshow-container");
+    slidesContainer.innerHTML = ''; // Clear existing content
 
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+    // Array of book covers (replace with actual cover filenames)
+    const bookCovers = ['book1_cover.jpg', 'book2_cover.jpg', 'book3_cover.jpg'];
+
+    for (let i = 0; i < bookCovers.length; i++) {
+        const slide = document.createElement('div');
+        slide.classList.add('book-slide', 'fade');
+
+        const img = document.createElement('img');
+        img.src = `books/${bookCovers[i]}`;
+        img.alt = `Book ${i + 1} Cover`;
+
+        const bookTitle = document.createElement('div');
+        bookTitle.classList.add('book-title');
+        bookTitle.innerHTML = `<a href="books/book${i + 1}.pdf" target="_blank">Book ${i + 1}</a>`;
+
+        slide.appendChild(img);
+        slide.appendChild(bookTitle);
+
+        slidesContainer.appendChild(slide);
     }
 
-    slides[slideIndex].style.display = "block";
+    // Display the current slide
+    slidesContainer.getElementsByClassName('book-slide')[slideIndex].style.display = 'block';
 }
 
 function plusSlides(n) {
     slideIndex += n;
 
-    const slides = document.getElementsByClassName("book-slide");
+    const slides = document.getElementsByClassName('book-slide');
 
     if (slideIndex >= slides.length) {
         slideIndex = 0;
@@ -24,15 +43,15 @@ function plusSlides(n) {
     showSlides();
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', function() {
     showSlides();
 
     // Attach click event handlers to navigation arrows
-    document.querySelector(".prev").addEventListener("click", function() {
+    document.querySelector('.prev').addEventListener('click', function() {
         plusSlides(-1);
     });
 
-    document.querySelector(".next").addEventListener("click", function() {
+    document.querySelector('.next').addEventListener('click', function() {
         plusSlides(1);
     });
 });
