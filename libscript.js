1c8a1733e9ea7af26fe7c09d2ea1df06273cafbf
yzycsn <154832604+yzycsn@users.noreply.github.com>
2024-01-07 16:50:03 -0500

    Update libscript.js

--- libscript.js
+++ libscript.js
@@ -5,13 +5,12 @@ const rightArrow = document.querySelector('.right');
 
 var sectionIndex = 0;
 
-rightArrow.addEventListener('click', function(){
-    sectionIndex = (section < 3) ? sectionIndex + 1 : 3;
-   slider.style.transform = 'translate('+ (sectionIndex) * -25 +'%)';
-});
-
 leftArrow.addEventListener('click', function(){
     sectionIndex = (section > 0) ? sectionIndex - 1 : 0;
    slider.style.transform = 'translate('+ (sectionIndex) * -25 +'%)';
 });
 
+rightArrow.addEventListener('click', function(){
+    sectionIndex = (section < 3) ? sectionIndex + 1 : 3;
+   slider.style.transform = 'translate('+ (sectionIndex) * -25 +'%)';
+});
