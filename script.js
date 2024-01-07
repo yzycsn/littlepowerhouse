930d048b56107f1c47f36cdc3c35f684d948b751
yzycsn <154832604+yzycsn@users.noreply.github.com>
2024-01-07 15:37:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
 document.getElementById('redirectToSection').addEventListener('click', function() {
-    // Replace 'subsection.html' with the actual path to your subsection
     window.location.href = 'library.html';
 });
