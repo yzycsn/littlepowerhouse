ea792ceec1201cf31c9108a163b70fffbbea6acd
yzycsn <154832604+yzycsn@users.noreply.github.com>
2024-01-07 15:23:17 -0500

    Add files via upload

---
+++ libscript.js
@@ -0,0 +1,8 @@
+const slider = document.querySelector('.slider');
+
+const leftArrow = document.querySelector('.left');
+const rightArrow = document.querySelector('.right');
+
+rightArrow.addEventListener('click', function(){
+   slider.style.transform = 'translate(-25%)';
+});
