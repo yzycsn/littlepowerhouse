6159660f0c323686cf10318146de585d7a865b62
yzycsn <154832604+yzycsn@users.noreply.github.com>
2024-01-08 16:24:50 -0500

    Update libscript.js

--- libscript.js
+++ libscript.js
@@ -2,15 +2,30 @@ const slider = document.querySelector('.slider');
 
 const leftArrow = document.querySelector('.left');
 const rightArrow = document.querySelector('.right');
-
+const indicatorParents = document.querySelector('.controls ul');
 var sectionIndex = 0;
 
+function setIndex(){
+    document.querySelector('.controls .selected').classList.remove('selected'); 
+    slider.style.transform = 'translate('+ (sectionIndex) * -25 + '%)';
+}
+
+document.querySelectorAll('.controls li').forEach(function(indicator, ind){
+    indicator.addEventListener('click', function(){
+        sectionIndex = ind;
+        setIndex();
+        indicator.classList.add('selected');
+    });
+});
+
 leftArrow.addEventListener('click', function(){
-    sectionIndex = (sectionIndex > 0) ? sectionIndex - 1 : 0;
-   slider.style.transform = 'translate('+ (sectionIndex) * -25 +'%)';
+    sectionIndex = (section > 0) ? sectionIndex - 1 : 0;
+    indicatorParents.children[sectionIndex].classList.add('selected');
+    setIndex();
 });
 
 rightArrow.addEventListener('click', function(){
-    sectionIndex = (sectionIndex < 3) ? sectionIndex + 1 : 3;
-   slider.style.transform = 'translate('+ (sectionIndex) * -25 +'%)';
+    sectionIndex = (section < 3) ? sectionIndex + 1 : 3;
+    indicatorParents.children[sectionIndex].classList.add('selected');
+    setIndex();
 });
